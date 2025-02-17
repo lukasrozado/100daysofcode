bb351a7d065bb01e8d40f7b5c2cc30e05335eded
Lukas <lukasrs13@gmail.com>
2025-02-17 14:20:15 -0300

    update02

--- day11/main.py
+++ day11/main.py
@@ -3,10 +3,8 @@ from art import logo
 
 # Logo
 print(logo)
-# Definir as cartas do baralho
 cards = [11, 2 , 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-# Inicializando as cartas dos jogadores
 player1_card1 = choice(cards)
 player1_card2 = choice(cards)
 player_initial_cards = [player1_card1, player1_card2]
@@ -17,12 +15,10 @@ computer_cards2 = choice(cards)
 computer_cards = [computer_cards1, computer_cards2]
 total_initial_cards_computer = sum(computer_cards)
 
-# Exibir as cartas iniciais
 print(f"Player Initial Cards: {player_initial_cards}")
 print(f"Total First Player: {total_initial_cards_player}")
 print(f"Computer First Card: {computer_cards1}")
 
-# Jogada do jogador
 while total_initial_cards_player <= 21:
     choices = input("Do you want another card? Press 'Y' to get one or 'N' to stop the game: ").lower()
 
@@ -41,7 +37,6 @@ while total_initial_cards_player <= 21:
     else:
         print("Invalid input! Please enter 'Y' or 'N'.")
 
-# Se o jogador não estourou, o computador joga
 if total_initial_cards_player <= 21:
     print(f"\nComputer Cards: {computer_cards}")
     
